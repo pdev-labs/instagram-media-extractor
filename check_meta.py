604b30dfb2b2c85b295962f90cfeb8a2528fe0f6
pdev <pdev@example.com>
2026-08-01 19:10:50 +0530

    chore: Clean up debugging scripts and unused files

--- check_meta.py
+++
@@ -1,6 +0,0 @@
-import instaloader
-L = instaloader.Instaloader()
-post = instaloader.Post.from_shortcode(L.context, "DZhnD1wjEgd")
-print("IS VIDEO:", post.is_video)
-print("URL:", post.url)
-print("VIDEO URL:", post.video_url)
